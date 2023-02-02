759eb1f5eacf5da948ace5a245b60cc24c3e1eee
ZengaOO <108860031+ZengaOO@users.noreply.github.com>
2023-02-02 15:13:32 +0300

    fixing bug

--- Encrypting/Services/IEncryptService.cs
+++ Encrypting/Services/IEncryptService.cs
@@ -5,6 +5,7 @@ namespace Encrypting.Services
     public interface IEncryptService
     {
         Task<byte[]> EncryptAsync(string clearText, string enteredText);
-        Task<string> DecryptAsync(byte[] encrypted, string passphrase);
+        // Task<string> DecryptAsync(byte[] encrypted, string passphrase);
+        Task<string> DecryptAsync(byte[] encrypted, string enteredText);
     }
 }
